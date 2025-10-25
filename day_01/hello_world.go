1492e3e797bfd5827feaee9eeca364a544f52ca0
yashgarg9606 <yashgarg9606@gmail.com>
2025-10-25 23:44:44 +0530

    Initial commit

---
+++ day_01/hello_world.go
@@ -0,0 +1,15 @@
+package main // every go file starts with a package declaration
+
+import "fmt"// include code from fmt (formatted I/O) package
+
+func main(){
+	fmt.Println("Hello, World!")
+}
+
+var x int = 10
+y:= 20 // shorthand for variable declaration and initialization
+
+/*
+Has control structures like if, for, switch, etc.
+does not have "while" loop
+*/
